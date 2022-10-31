59a42ddc8c3ed9f83cab30fac525fbdc064c249e
Kyuwoong Lee <kwooler@gmail.com>
2022-10-30 20:42:34 -0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -4,13 +4,13 @@ import main
 
 def test_main():
 	str = 'Python Programming'
-	printfunction1(*str)
-	printfunction2(str)
+	main.printfunction1(*str)
+	main.printfunction2(str)
 
 	morestr = 'C++ Programming'
-	printfunction1(str, morestr)
+	main.printfunction1(str, morestr)
     
-	printfunction1('a', 'b', 'c', 'd', 'e')
+	main.printfunction1('a', 'b', 'c', 'd', 'e')
 	# assert v1 == -10, "Min value does not match"
 	# assert v2 == 5, "Max value does not match"
     
