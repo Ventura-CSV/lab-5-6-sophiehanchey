2efb2e3f9312708239a5d7be01e683011c469f50
Kyu Lee <kwooler@gmail.com>
2023-03-26 22:38:22 -0700

    L56

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 
 
 def printfunction1(*str):
-	print (str)
-	for v in str:
-		print (v, end='')
-	print()
+    print(str)
+    for v in str:
+        print(v, end=' ')
+    print()
+
 
 def printfunction2(str):
-	for v in str:
-		print (v, end='')
-	print()
+    for v in str:
+        print(v, end='')
+    print()
 
 
 def main():
-	str = 'Python Programming'
-	printfunction1(*str)
-	printfunction2(str)
+    str = 'Python Programming'
+    printfunction1(*str)
+    printfunction2(str)
 
-	morestr = 'C++ Programming'
-	printfunction1(str, morestr)
-	# printfunction2(str, morestr)  # Error
+    morestr = 'C++ Programming'
+    printfunction1(str, morestr)
+    # printfunction2(str, morestr)  # Error
 
-main()
+
+if __name__ == '__main__':
+    main()
